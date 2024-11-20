547f4b1dcb8001fa7a82d89597093ef046896101
Saira Nasir <sairanasir10014@gmail.com>
2024-11-20 20:00:02 +0000

    add commenting for understanding

---
+++ 01_basics/datatype.js
@@ -0,0 +1,2 @@
+// lets begin with data type
+
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,10 +1,34 @@
+//question 1 : HOW TO PRINT IN THIS JAVASCRIPT LANGUAGE
+
 console.log ("Saira Nasir")
 console.log("HI I AM NEW HERE")
-console.log("Help me And Pray For me because i am very nervouse wether i will be able to do it Or NOt")
+console.log("Help me And Pray For me because i am very nervouse wether  i will be able to do it Or NOt")
 
 
-//var const and let
+
+//Q2: variable DECLARATION FOR ALL DATA TYPES 
+               //var const and let
+
 const accountId = 12345
 let accountEmail ="sairanasir10014@gmail.com"
-var accuntPassword = "12345678"
+var accountPassword = "12345678"
 accountCity = "Sheikhupura"
+
+
+
+                    // MINI NOTES FOR BETTER UNDERSTANDING
+
+                    /* 1...DONT USE VAR PREFER USING LET VARIABE 
+                           BECAUSE ISSUE IN SCOPE .LET keyword  HANDLE IT BETTER
+
+                    2... JAVASCRIPT VARIABLES CAN ALSO MEMORY ACCESS
+                         WITHOUT USING KEYWORDS BUT TS NOT A SAFE APROACH */
+
+console.log(accountId)
+// accountId=333 trying to change the constant but it will not happening
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity);
+console.table([accountId,accountEmail,accountPassword,accountCity])//Toprint data in table form
+
+
