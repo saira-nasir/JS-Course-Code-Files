edcb79fa1504407aa610c5d50711e4155fa6d40d
Saira Nasir <sairanasir10014@gmail.com>
2024-11-20 19:20:59 +0000

    Variables Concep and code

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,3 +1,10 @@
 console.log ("Saira Nasir")
 console.log("HI I AM NEW HERE")
 console.log("Help me And Pray For me because i am very nervouse wether i will be able to do it Or NOt")
+
+
+//var const and let
+const accountId = 12345
+let accountEmail ="sairanasir10014@gmail.com"
+var accuntPassword = "12345678"
+accountCity = "Sheikhupura"
