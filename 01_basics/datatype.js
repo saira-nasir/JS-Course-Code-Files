7556a9d8643498a5aa6f9a80281de45b49393440
Saira Nasir <sairanasir10014@gmail.com>
2024-12-25 17:39:16 +0000

    datatypes

--- 01_basics/datatype.js
+++ 01_basics/datatype.js
@@ -1,2 +1,22 @@
 // lets begin with data type
 
+"use strict"//
+// alert(2+3) we cannot use this bcz node doesnt accept this
+// code readability should be high
+
+console.log('this is datatype file');
+
+
+//int 7987
+//string => "kwhgq"
+//boolean false true
+//bigint for big application
+//null khali jaga 
+//undefined matlab value assign ni hoi
+//symbol => unique
+//object
+
+console.log(typeof "saira")
+console.log(typeof null)
+console.log(typeof undefined)
+
